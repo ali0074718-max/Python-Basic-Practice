9b964f12e091494f675e1b8362f89da62d10247e
ali0074718-max <ali0074718@gmail.com>
2026-07-27 18:52:19 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+print("Hello World")
+a=5
+b=10
+c=a+b
+print("c =" , c)
+print(33+23)
+print("33+23")
+print(2*5)
+print("Memory Address of a (5):" ,id(a))
+print("Memory Address of b (10):" ,id(b))
+x=5
+print("Memory Address of a (5):" ,id(x))
