4d55b93d6a4725f69bdb07867ba5ae539f653b62
ali0074718-max <ali0074718@gmail.com>
2026-07-29 08:47:22 +0500

    Add files via upload

---
+++ stringc.py
@@ -0,0 +1,27 @@
+
+#String Concatenation Without Space
+F_name = "Ali"
+L_name = "Haider"
+Full_name = F_name + L_name
+print("Without Space:" ,Full_name)
+
+#String Concatenation Without Space
+full_name_with_space = F_name + " " + L_name
+print("With Space:" ,full_name_with_space )
+
+#Difference Between Math Addition and String Concatenation
+num1 = 10
+num2 = 25
+sum = num1 + num2
+print("Math Addition:" ,sum)
+
+a = "10"
+b = "20"
+print("String Concatenation:" ,a + b)
+
+#Type Conversion (Converting Number to String)
+age = 22
+age_message = "My age is: " + str(age)
+print(age_message)
+
+
