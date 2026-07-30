581cae188b6b7c2e82d380221f9251500c407a39
ali0074718-max <ali0074718@gmail.com>
2026-07-30 18:31:41 +0500

    Add files via upload

---
+++ arithmetic_operators.py
@@ -0,0 +1,10 @@
+#Arithmetic Operators in Python
+a = 10
+b = 5
+print("Addition: " , a+b)
+print("Subtraction: " , a-b)
+print("Multiplication: " , a*b)
+print("Division: " , a/3)
+print("Modulus: " , a%3)
+print("Exponentiation: " , 5**2)
+print("Floor Division: " , a//3)
