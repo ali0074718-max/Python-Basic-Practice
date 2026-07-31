1ba01d0666155ec212c580d73c582d3e1c42032f
ali0074718-max <ali0074718@gmail.com>
2026-07-31 20:16:14 +0500

    Add files via upload

---
+++ assignment_operators.py
@@ -0,0 +1,19 @@
+x = 10
+print("Initial value of x:" , x)
+
+#Add and Assign (+=)
+x = x + 5    #Same as: x += 5
+print("After x = x + 5:",x)
+
+#Subtract and Assign (-=)
+x = x - 5   #Same as: x -= 5
+print("After x = x - 5:",x)
+
+#Multiple and Assign (*=)
+x = x * 3  #Same as x *= 3
+print("After x = x * 3:",x)
+
+#Divide and Assign (/=)
+x = x / 4  #Same as: x /= 4
+print("After x = / 4:", x)
+
