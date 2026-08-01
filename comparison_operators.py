b16344c4188440cf55fe285d1fbc63114e2c67e8
ali0074718-max <ali0074718@gmail.com>
2026-08-01 21:18:17 +0500

    Added comparison operators practice program

---
+++ comparison_operators.py
@@ -0,0 +1,24 @@
+a = 10
+b = 5
+
+# Equal to (==)
+print("Is a equal to b?", a == b)
+
+# Not equal to (!=)
+print("Is a not equal to b? ", a != b)
+
+# Greater than (>)
+print("Is a greater than b?", a > b)
+
+# Less than (<)
+print("Is a less than b?", a < b)
+
+# Greater than or equal to (>=)
+print("Is a greater than or equal to b?", a >= b)
+
+# Less than or equal to (<=)
+print("Is a less than or equal to b?", a <= b)
+
+#
+
+
