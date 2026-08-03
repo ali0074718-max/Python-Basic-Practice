7d74df249b0d48c15a3375652cd70e1dafec19db
ali0074718-max <ali0074718@gmail.com>
2026-08-03 11:49:49 +0500

    Added membership operators practice program

---
+++ membership_operators.py
@@ -0,0 +1,26 @@
+# Checking in a string (Text)
+text = "Hello"
+print("Is 'H' in text?", "H" in text)
+print("Is 'P' in text?", "P" in text)
+
+# Not in
+print("Is 'e' not in text?", "e" not in text)
+print("Is 'A' not in text?", "A" not in text )
+
+# Check in a list (Group of items)
+fruit = ["apple", "banana", "mango"]
+print("Is 'apple' in fruit?", "apple" in fruit)
+print("Is 'Guava' in fruit?", "Guava" in fruit)
+
+# Not in
+print("Is 'Pear' not in fruit?", "Pear" not in fruit)
+print("Is 'mango' not in fruit?", "mango" not in fruit)
+
+# Check in a numbers list
+numbers = [10,20,30,40,50]
+print(25 in numbers)
+print(40 in numbers)
+
+# Not in
+print(50 not in numbers)
+print(15 not in numbers)
