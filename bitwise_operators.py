9d47bd38ff6ecc0160ac7cc6de679cda456f50be
ali0074718-max <ali0074718@gmail.com>
2026-08-05 16:57:47 +0500

    Added bitwise operators practice program

---
+++ bitwise_operators.py
@@ -0,0 +1,31 @@
+x = 8
+y = 10
+#Check binary value of x and y
+print("Binary of x:", bin(x))
+print("Binary of y:", bin(y))
+
+#Bitwise operation (Binary and Decimal Results)
+#Bitwise AND
+print("Bitwise AND (x & y) Binary:", bin(x & y))
+print("Bitwise AND (x & y) Decimal:", x & y)
+
+#Bitwise OR
+print("Bitwise OR (x | y) Binary:", bin(x | y) )
+print("Bitwise OR (x |y) Decimal:", x | y)
+
+#Bitwise XOR
+print("Bitwise XOR (x ^ y) Binary:",bin(x ^ y))
+print("Bitwise XOR (x ^ y) Decimal:", x ^ y)
+
+#Bitwise NOT
+print("Bitwise NOT (~x) Binary:", bin(~x))
+print("Bitwise NOT (~x) Decimal:", ~x)
+
+#Shift Operatiopns
+print("Left Shift (x << 1) Binary:", bin(x << 1))
+print("Left Shift (x << 1) Decimal:", x << 1)
+
+print("Right Shift (x >> 1) Binary:", bin(x >> 1))
+print("Right Shift () x >> 1 ) Decimal:", x >> 1)
+
+print("Bitwise Hexa (x & y) Hexa: ",hex(x & y))
