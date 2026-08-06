f157934b0bd507ba4208bb73f587123d8925fa9d
ali0074718-max <ali0074718@gmail.com>
2026-08-06 19:29:28 +0500

    Add user input and typecasting practice code

---
+++ user_input_and_typecasting.py
@@ -0,0 +1,14 @@
+# User input
+name = input("Enter your name:")
+country = input("Enter your country name:")
+print("Name:", name)
+print("Country:", country)
+
+#type casting
+age = int(input("Enter your age:"))
+print(age)  #convert strint into integer
+
+# Using typecasting sum calculation
+num1 = int(input("Enter the value1:"))
+num2 = int(input("Enter the value2:"))
+print("Sum: ", num1 + num2)
