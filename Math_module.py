8f2e452fd737396c25c34565bc56f991d8c6beab
ali0074718-max <ali0074718@gmail.com>
2026-08-15 18:55:45 +0500

    Add Python math module practice script
    Implemented basic math module functions (ceil, floor, sqrt, fabs, factorial,fsum).

---
+++ Math_module.py
@@ -0,0 +1,26 @@
+# Import Math Library
+import math
+# Math.ceil(x)
+x = 10.2
+print(math.ceil(x))
+
+# Math.floor(x)
+a = 12.5
+print(math.floor(a))
+
+# Math.factorial
+b = 5
+print(math.factorial(b))
+
+# Math.fsum(list/tuple)
+l = [10,20,30,40]
+print(math.fsum(l))
+
+# Math.sqr(x)
+x = 25
+print(math.sqrt(x))
+print(math.sqrt(81))
+
+# Math.fabs(x)
+f = -10
+print(math.fabs(f))
