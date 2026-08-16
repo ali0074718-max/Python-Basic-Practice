995f522d501711a03c95be33058627c3621ae3ee
ali0074718-max <ali0074718@gmail.com>
2026-08-16 16:45:31 +0500

    Add Python random module practice program
    Practice Python random module functions (randint, randrange, choice, shuffle, uniform)

---
+++ Random_module.py
@@ -0,0 +1,22 @@
+import random
+n = random.randint(3,9)
+print(n)
+
+n1 = random.randrange(2,8)
+print(n1)
+
+l = [1,2,3,4,5,6]
+lc = random.choice(l)
+print(lc)
+# OR
+print(random.choice(l))
+
+r = random.random()
+print(r)
+
+l2 = [10,20,30,40,50]
+random.shuffle(l2)
+print(l2)
+
+u = random.uniform(1,7)
+print(u)
